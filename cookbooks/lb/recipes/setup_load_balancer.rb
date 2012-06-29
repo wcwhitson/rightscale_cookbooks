1267f1bb1f993475b6a4bd914b83c646ab8ef7ff
Neve <n.stakanov@tut.by>
2012-06-29 12:13:42 +0300

    [lb][lb_haproxy] dev: .hash firs element

--- cookbooks/lb/recipes/setup_load_balancer.rb
+++ cookbooks/lb/recipes/setup_load_balancer.rb
@@ -17,7 +17,7 @@ log "  Install load balancer"
 
 # In the 'install' action, the name is not used, but the provider from default recipe is needed.
 # Any vhost name set with provider can be used. Using first one in list to make it simple.
-lb vhosts(POOL_NAMES).default(0) do
+lb vhosts(POOL_NAMES).keys[0] do
   action :install
 end
 
